e7717761e840ced21e62a2c290b6da257782d34a
b_rasulov <b.rasulov@uzinfocom.uz>
2025-12-18 18:00:08 +0500

    fix(layout): adjust padding in ContentWrapper and update contentWidth in themeConfig

--- src/@core/layouts/VerticalLayout.tsx
+++ src/@core/layouts/VerticalLayout.tsx
@@ -38,7 +38,7 @@ const MainContentWrapper = styled(Box)<BoxProps>({
 const ContentWrapper = styled('main')(({ theme }) => ({
   flexGrow: 1,
   width: '100%',
-  padding: theme.spacing(6),
+  padding: theme.spacing(2),
   transition: 'padding .25s ease-in-out',
   [theme.breakpoints.down('sm')]: {
     paddingLeft: theme.spacing(4),
